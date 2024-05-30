4cdaf74d3450d793e6b6e52f7e07cc5e57b98aa1
jorted-runner <jorted.runner1996@gmail.com>
2024-05-30 16:29:52 -0500

    Register Account and Set up Admin Route Decorator

--- app.py
+++ app.py
@@ -17,9 +17,16 @@ from sqlalchemy import func
 from datetime import datetime
 from sqlalchemy import and_
 
+from retrieval import Retrieval
+from data_validation import Data_Validation
+
+load_dotenv()
+RETRIEVER = Retrieval()
+VALIDATOR = Data_Validation()
+
 app = Flask(__name__)
 app.config.from_object(app_config)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = app_config.SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = app_config.DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -29,6 +36,7 @@ class User(UserMixin, db.Model):
     fname = db.Column(db.String(100), nullable=False)
     lname = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(250), unique=True, nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(250), nullable=True)
     sign_up_date = db.Column(db.DateTime, default=datetime.now)
     profile_type = db.Column(db.String(100), nullable=False, default='client')
@@ -41,6 +49,7 @@ class Address(db.Model):
     city = db.Column(db.String(250), nullable=False)
     state = db.Column(db.String(250), nullable=False)
     zip_code = db.Column(db.String(15), nullable=False)
+    trash_day = db.Column(db.String(15), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     trash_can_data = db.relationship('Trash_Can_Data', backref='address', uselist=False, cascade="all, delete-orphan")
 
@@ -48,8 +57,9 @@ class Trash_Can_Data(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     num_cans = db.Column(db.Integer, nullable=False)
     location = db.Column(db.String(250), nullable=False)
-    gate_garage_code = db.Column(db.String(250), nullable=False)
-    pet_info = db.Column(db.String(250), nullable=False)
+    gate_garage_code = db.Column(db.String(250), nullable=True)
+    pet_info = db.Column(db.String(250), nullable=True)
+    notes = db.Column(db.String(250), nullable=True)
     address_id = db.Column(db.Integer, db.ForeignKey('address.id'), unique=True, nullable=False)
 
 login_manager = LoginManager()
@@ -62,30 +72,104 @@ with app.app_context():
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-@app.errorhandler(404)
-def not_found(e):
-    return render_template('404.html', e=e)
+def admin_only(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if current_user.profile_type != 'client' or current_user.email in app_config.ADMIN_EMAILS:
+            return abort(403)
+        return f(*args, **kwargs)
+    return decorated_function
 
-@app.errorhandler(403)
-def not_found(e):
-    return render_template('403.html', e=e)
+# @app.errorhandler(404)
+# def not_found(e):
+#     return render_template('404.html', e=e)
 
-@app.errorhandler(Exception)
-def handle_exception(e):
-    return render_template('error.html', e=e), 500
+# @app.errorhandler(403)
+# def not_found(e):
+#     return render_template('403.html', e=e)
+
+# @app.errorhandler(Exception)
+# def handle_exception(e):
+#     return render_template('error.html', e=e), 500
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/register')
+@app.route('/register', methods=['POST', 'GET'])
 def register():
+    if request.method == "POST":
+        first_name = VALIDATOR.clean_input(request.form.get('first-name'))
+        last_name = VALIDATOR.clean_input(request.form.get('last-name'))
+        email = VALIDATOR.clean_input(request.form.get('email'))
+        phone_num = VALIDATOR.clean_input(request.form.get('phone'))
+        password = request.form.get('password_conf')
+        house_num = VALIDATOR.clean_input(request.form.get('houseNum'))
+        street_address = VALIDATOR.clean_input(request.form.get('street'))
+        city = VALIDATOR.clean_input(request.form.get('city'))
+        state = VALIDATOR.clean_input(request.form.get('state'))
+        zipcode = VALIDATOR.clean_input(request.form.get('zipcode'))
+        trash_day = VALIDATOR.clean_input(request.form.get('trash_day'))
+        num_cans = VALIDATOR.clean_input(request.form.get('number-of-cans'))
+        location = VALIDATOR.clean_input(request.form.get('location'))
+        gate_code = VALIDATOR.clean_input(request.form.get('gate-code'))
+        pet_info = VALIDATOR.clean_input(request.form.get('pets'))
+        notes = VALIDATOR.clean_input(request.form.get('notes'))
+
+        if RETRIEVER.get_user(User, email, 'email'):
+            flash("You've already signed up with that email, <a href='" + url_for('login') + "'>log in</a> instead!")
+            return redirect(url_for('login'))
+        
+        new_user_password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
+        
+        new_user = User(
+            fname=first_name, 
+            lname=last_name,
+            email=email,
+            phone=phone_num,      
+            password=new_user_password
+        )
+        db.session.add(new_user)
+        
+        db.session.commit()
+        new_address = Address(
+            house_num=house_num,
+            street_address=street_address,
+            city=city,
+            state=state,
+            zip_code=zipcode,
+            trash_day=trash_day,
+            user_id=new_user.id
+        )
+        db.session.add(new_address)
+        db.session.commit()
+
+        new_trash_can = Trash_Can_Data(
+            num_cans=num_cans, 
+            location=location, 
+            gate_garage_code=gate_code, 
+            pet_info=pet_info, 
+            notes=notes,
+            address_id=new_address.id
+        )
+        db.session.add(new_trash_can)
+        db.session.commit()
+
+        login_user(new_user)
+        return redirect(url_for('manage'))
     return render_template('register.html')
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+@login_required
+@app.route('/manage')
+def manage():
+    user = User.query.filter_by(id=current_user.id).first()
+    return render_template('manage.html', user=user)
+
+@admin_only
 @app.route('/admin')
 def admin():
     return render_template('admin.html')
--- app_config.py
+++ app_config.py
@@ -1,1 +1,6 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
 DATABASE_URI = 'sqlite:///can2curb.sqlite3'
+ADMIN_EMAILS = os.environ.get('ADMIN_EMAILS')
+SECRET_KEY = os.environ.get("SECRET_KEY")
